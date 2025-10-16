4ea068c2029890037d3cc2dc594352ac2a8619fb
flancitoo01arg <franco115.fg@gmail.com>
2025-10-16 16:23:07 -0300

    Login del Instituto Politécnico Formosa funcionando con Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import LoginForm from './components/LoginForm';
+import './App.css'; // ← Solo si usas App.css para Tailwind
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenidos a: 
-          <br/>
-          <code>SMART - TUTOR</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <LoginForm />;
 }
 
 export default App;
